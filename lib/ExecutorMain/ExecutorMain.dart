dfa42ef2d25cbc673b0a29a3dd459bb88b9205f9
Eric Apostal <ehapostal04@gmail.com>
2023-07-12 08:59:09 -0400

    wip monaco

--- lib/ExecutorMain/ExecutorMain.dart
+++ lib/ExecutorMain/ExecutorMain.dart
@@ -2,11 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_code_editor/flutter_code_editor.dart';
 import 'package:flutter_highlight/themes/monokai.dart';
 import 'package:highlight/languages/lua.dart';
+import "package:webview_universal/webview_universal.dart";
 
 final controller = CodeController(
   text: '', // Initial code
   language: lua,
 );
+
+class Monaco extends StatefulWidget {
+  const Monaco({super.key});
+
+  @override
+  State<Monaco> createState() => _MonacoState();
+}
+
+class _MonacoState extends State<Monaco> {
+  WebViewController webViewController = WebViewController();
+
+  @override
+  void initState() {
+    super.initState();
+    webViewController.init(
+      context: context,
+      setState: setState,
+      uri: Uri.parse("https://flutter.dev"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        controller: webViewController,
+      ),
+    );
+  }
+}
 
 class TextArea extends StatelessWidget {
   const TextArea({Key? key}) : super(key: key);
@@ -20,18 +51,7 @@ class TextArea extends StatelessWidget {
           color: Color(0XFF3B4348),
           borderRadius: BorderRadius.all(Radius.circular(6)),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: CodeTheme(
-            data: CodeThemeData(styles: monokaiTheme),
-            child: SingleChildScrollView(
-              child: CodeField(
-                background: const Color(0XFF3B4348),
-                controller: controller,
-              ),
-            ),
-          ),
-        ),
+        child: Padding(padding: const EdgeInsets.all(8), child: Monaco()),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import bitsdojo_window_macos
+import path_provider_foundation
 import screen_retriever
 import url_launcher_macos
+import webview_universal
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebviewUniversalPlugin.register(with: registry.registrar(forPlugin: "WebviewUniversalPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
