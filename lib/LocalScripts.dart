0272ebe278c7898ab6363b984026dcbd84d2aabd
Eric Apostal <ehapostal04@gmail.com>
2023-08-19 13:45:53 -0400

    chatgpt file explorer

--- lib/LocalScripts.dart
+++ lib/LocalScripts.dart
@@ -1,23 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-void main() {
-  runApp(MaterialApp(home: LocalScripts()));
-}
+import 'package:path_provider/path_provider.dart';
 
 class FileItem {
   final String name;
   final bool isFolder;
-  final Color color;
+  final FileSystemEntity entity;
 
-  FileItem({required this.name, required this.isFolder, required this.color});
+  FileItem({required this.name, required this.isFolder, required this.entity});
 }
 
 class FolderIcon extends StatefulWidget {
-  const FolderIcon({Key? key, required this.fileItem}) : super(key: key);
+  const FolderIcon(
+      {Key? key, required this.fileItem, required this.onDoubleTap})
+      : super(key: key);
 
   final FileItem fileItem;
+  final VoidCallback onDoubleTap;
 
   @override
   State<FolderIcon> createState() => _FolderIconState();
@@ -28,76 +30,119 @@ class _FolderIconState extends State<FolderIcon> {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      child: Container(
-        decoration: BoxDecoration(
-          color: _isHovered ? const Color(0xFF222735) : const Color(0xFF13141A),
-          borderRadius: const BorderRadius.all(Radius.circular(2)),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: SizedBox(
-                width: 23,
-                height: 23,
-                child: SvgPicture.asset(
-                  widget.fileItem.isFolder
-                      ? "assets/folder.svg"
-                      : "assets/document.svg",
-                  key: const ValueKey<String>("assets/folder.svg"),
-                  colorFilter:
-                      ColorFilter.mode(widget.fileItem.color, BlendMode.srcIn),
-                  semanticsLabel:
-                      widget.fileItem.isFolder ? 'Folder' : 'Document',
+    return GestureDetector(
+      onDoubleTap: widget.fileItem.isFolder ? widget.onDoubleTap : null,
+      child: MouseRegion(
+        onEnter: (_) => setState(() => _isHovered = true),
+        onExit: (_) => setState(() => _isHovered = false),
+        child: Container(
+          decoration: BoxDecoration(
+            color:
+                _isHovered ? const Color(0xFF222735) : const Color(0xFF13141A),
+            borderRadius: const BorderRadius.all(Radius.circular(2)),
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 4),
+                child: SizedBox(
+                  width: 23,
+                  height: 23,
+                  child: SvgPicture.asset(
+                    widget.fileItem.isFolder
+                        ? "assets/folder.svg"
+                        : "assets/document.svg",
+                    key: const ValueKey<String>("assets/folder.svg"),
+                    colorFilter:
+                        ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                    semanticsLabel:
+                        widget.fileItem.isFolder ? 'Folder' : 'Document',
+                  ),
                 ),
               ),
-            ),
-            Text(
-              widget.fileItem.name,
-              style:
-                  GoogleFonts.inter(color: widget.fileItem.color, fontSize: 16),
-            ),
-          ],
+              Text(
+                widget.fileItem.name,
+                style: GoogleFonts.inter(color: Colors.white, fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class LocalScripts extends StatelessWidget {
-  const LocalScripts({Key? key}) : super(key: key);
+class LocalScripts extends StatefulWidget {
+  @override
+  _LocalScriptsState createState() => _LocalScriptsState();
+}
+
+class _LocalScriptsState extends State<LocalScripts> {
+  late Directory currentDirectory;
+  List<FileSystemEntity> files = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initCurrentDirectory();
+  }
+
+  Future<void> _initCurrentDirectory() async {
+    final directory = await getApplicationDocumentsDirectory();
+    setState(() {
+      currentDirectory = directory;
+      files = directory.listSync();
+    });
+  }
+
+  void _updateFiles(Directory directory) {
+    setState(() {
+      currentDirectory = directory;
+      files = directory.listSync();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Here's your mock data
-    List<FileItem> files = [
-      FileItem(name: "Scripts", isFolder: true, color: Color(0xFF23CAFF)),
-      FileItem(name: "Workspace", isFolder: true, color: Color(0xFFFFF968)),
-      FileItem(name: "AutoExec", isFolder: true, color: Color(0xFF77EF91)),
-      FileItem(name: "readme.txt", isFolder: false, color: Color(0xFFD6D6D6)),
-    ];
-
     return SizedBox(
-        width: 120,
-        child: Column(
-          children: [
-            const SizedBox(height: 2),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "~/",
-                textAlign: TextAlign.start,
-                style: GoogleFonts.inter(
-                    color: const Color.fromARGB(255, 198, 198, 198),
-                    fontSize: 16),
-              ),
+      width: 250,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 2),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              currentDirectory.path,
+              textAlign: TextAlign.start,
+              style: GoogleFonts.inter(
+                  color: const Color.fromARGB(255, 198, 198, 198),
+                  fontSize: 16),
             ),
-            const SizedBox(height: 4),
-            ...files.map((file) => FolderIcon(fileItem: file)).toList(),
-          ],
-        ));
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: files.length,
+              itemBuilder: (context, index) {
+                final fileEntity = files[index];
+                final isFolder = fileEntity is Directory;
+                return FolderIcon(
+                  fileItem: FileItem(
+                    name: fileEntity.uri.pathSegments.last,
+                    isFolder: isFolder,
+                    entity: fileEntity,
+                  ),
+                  onDoubleTap: () {
+                    if (isFolder) {
+                      _updateFiles(fileEntity as Directory);
+                    }
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
